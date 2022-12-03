a6fc038de43ea6258995f6aa0a1c9276cd6028dd
Sofia92 <sofiasosaperez@outlook.com>
2022-12-03 13:40:19 -0600

    changed HTML format

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
 
-const timeEl = document.getElementById('time');
-const dateEl = document.getElementById('date');
-const currentWeatherItemsEl = document.getElementById('current-weather-items');
-const timezone = document.getElementById('time-zone');
-const countryEl = document.getElementById('country');
-const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTempEl = document.getElementById('current-temp');
 
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-//API key
-const API ='1f43674c9358ffdf830ce1265be2b12c';
 
-setInterval(() => {
-    const time = new Date();
-    const month =  time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour 
-    const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ '' + `<span id="am-pm">${ampm}</span>`
 
-    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+// const timeEl = document.getElementById('time');
+// const dateEl = document.getElementById('date');
+// const currentWeatherItemsEl = document.getElementById('current-weather-items');
+// const timezone = document.getElementById('time-zone');
+// const countryEl = document.getElementById('country');
+// const weatherForecastEl = document.getElementById('weather-forecast');
+// const currentTempEl = document.getElementById('current-temp');
+
+
+// const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// //API key
+// const API ='1f43674c9358ffdf830ce1265be2b12c';
+
+// setInterval(() => {
+//     const time = new Date();
+//     const month =  time.getMonth();
+//     const date = time.getDate();
+//     const day = time.getDay();
+//     const hour = time.getHours();
+//     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour 
+//     const minutes = time.getMinutes();
+//     const ampm = hour >=12 ? 'PM' : 'AM'
+
+//     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ '' + `<span id="am-pm">${ampm}</span>`
+
+//     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
     
-}, 1000); //every 1 second
+// }, 1000); //every 1 second
 
-getWeatherData()
-function getWeatherData () {
-    navigator.geolocation.getCurrentPosition((success) => {
-        console.log(sucess);
+// getWeatherData()
+// function getWeatherData () {
+//     navigator.geolocation.getCurrentPosition((success) => {
+//         console.log(sucess);
 
-        let {latitude, longitude } = success.coords;
-//api link
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
-            console.log(data)
-            showWeatherData(data);
-        })
-    })
-}
+//         let {latitude, longitude } = success.coords;
+// //api link
+//         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+//             console.log(data)
+//             showWeatherData(data);
+//         })
+//     })
+// }
 
-function showWeatherData (data){
-let{ humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+// function showWeatherData (data){
+// let{ humidity, pressure, sunrise, sunset, wind_speed} = data.current;
 
-}
+// currentWeatherItemsEl.innerHTML = 
+// `<div class="weather-item">
+// <div>Humidity</div>
+// <div>${humidity}%</div>
+// </div>
+// <div class="weather-item">
+// <div>Pressure</div>
+// <div>${pressure}</div>
+// </div>
+// <div class="weather-item">
+// <div>Wind Speed</div>
+// <div>${wind_speed}</div>
+// </div>
+// <div class="weather-item">
+// <div>Wind Speed</div>
+// <div>${wind_speed}</div>
+// </div>
+// `;
+
+// }
